5e00dab40042437d62a47f40ac520ed514678a4a
Srushti Lohiya <69888985+srushtilohiya@users.noreply.github.com>
2024-11-27 19:15:08 +0530

    Update Main.java 1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,9 +4,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-
-    // A simple method to greet someone
-    public String greet(String name) {
-        return "Hello, " + name + "!";
-    }
 }
