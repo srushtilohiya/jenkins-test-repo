94132b1a60f8c3887f333b6514bb5ddd2c9b2c53
Srushti Lohiya <69888985+srushtilohiya@users.noreply.github.com>
2024-11-27 20:55:12 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,4 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    // Add the greet method if it doesn't exist
+    public String greet(String name) {
+        return "Hello, " + name + "!";
+    }
 }
